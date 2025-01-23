4a9bec8094be1c03126ccf29d45133e0eb8d0937
Leonardo Mercado <lamercadob1@gmail.com>
2025-01-22 21:44:25 -0500

    eliminacion de mongoos y base de datos de mongo, remplazado por firebase/firestore

--- SaiBackNodejs/index.js
+++ SaiBackNodejs/index.js
@@ -1,17 +1,10 @@
 'use strict'
 
-var mongoose = require('mongoose');
 var app = require('./app');
 var port = 3900;
 
+//inicializa el servidor
+app.listen(port,() =>{
+  console.log('servidor corriendo en http://localhost:'+port);
+});
 
-mongoose.set('useFindAndModify',false);
-mongoose.Promise= global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/apirestsai', { useNewUrlParser: true })
-      .then(()=>{
-        console.log('La conexion a la base de datos se a realizado bien!!');
-        //creamos el servidor y ponemos las peticiones https
-        app.listen(port,() =>{
-          console.log('servidor corriendo en http://localhost:'+port);
-        });
-      });
